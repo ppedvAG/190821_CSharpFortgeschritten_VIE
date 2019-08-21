ac66a91494fb25692969624a07203f170de52277
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-21 10:59:42 +0200

    Selbstgeschriebener Stack erstellt

---
+++ Generics_Demo/Generics_Demo/ObjectStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Demo
+{
+    class ObjectStack
+    {
+        public ObjectStack() : this(4) { }
+        public ObjectStack(int capacity)
+        {
+            pointer = 0;
+            data = new object[capacity]; // Standardgröße im .NET Framework
+        }
+
+        private object[] data;
+        private int pointer;
+
+        public void Push(object item)
+        {
+            if (pointer == data.Length)
+            {
+                var newData = new object[data.Length * 2];
+                Array.Copy(data, newData, data.Length);
+                data = newData;
+            }
+
+            data[pointer] = item;
+            pointer++;
+        }
+        public object Pop()
+        {
+            if (pointer > 0)
+            {
+                pointer--;
+                var result = data[pointer];
+                data[pointer] = null;
+                return result;
+            }
+            else
+                throw new InvalidOperationException("Stack ist leer");
+        }
+
+    }
+}
--- Generics_Demo/Generics_Demo/Program.cs
+++ Generics_Demo/Generics_Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,43 @@ namespace Generics_Demo
     {
         static void Main(string[] args)
         {
+            #region Generics aus "Anwendersicht"
+            // Generics aus "Anwendersicht"
+            // List<int> meineZahlen = new List<int>();
+            // meineZahlen.Add(12);
+            // meineZahlen.Add(7);
+            // meineZahlen.Add(3);
+
+            // foreach (var item in meineZahlen)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            // Variante vor .NET 2.0
+            // ArrayList alteZahlenListe = new ArrayList();
+            // alteZahlenListe.Add(123);
+            // alteZahlenListe.Add("123");
+            // alteZahlenListe.Add(null);
+            #endregion
+
+            ObjectStack os = new ObjectStack();
+            os.Push(7);
+            os.Push("12");
+            os.Push(3);
+            os.Push(99);
+
+            os.Push(12345);
+
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+            Console.WriteLine(os.Pop());
+
+            Console.WriteLine(os.Pop()); // Exception
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
