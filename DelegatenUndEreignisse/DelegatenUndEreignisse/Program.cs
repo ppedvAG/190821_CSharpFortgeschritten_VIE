b014eaa59abd7a020cac0cf3504733abb559b9eb
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-21 14:35:39 +0200

    Delegate - Schlüsselwort

--- DelegatenUndEreignisse/DelegatenUndEreignisse/Program.cs
+++ DelegatenUndEreignisse/DelegatenUndEreignisse/Program.cs
@@ -8,8 +8,32 @@ namespace DelegatenUndEreignisse
 {
     class Program
     {
+        // Delegate-Type: Signatur (Rückgabetyp, Name, Parameter)
+        public delegate void MeinDelegat();
+        public delegate void MeinDelegat2(int zahl);
         static void Main(string[] args)
         {
+            #region Delegate - Schlüsselwort
+            //MeinDelegat del = new MeinDelegat(A);
+            //del += B;
+            //del();
+
+            //MeinDelegat2 del2 = C; // Alternative Schreibweise
+            //del2(99); 
+            #endregion
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+
+        static void A()
+        {
+            Console.WriteLine("A");
+        }
+        static void B() => Console.WriteLine("B");
+        static void C(int zahl)
+        {
+            Console.WriteLine($"C{zahl}");
         }
     }
 }
