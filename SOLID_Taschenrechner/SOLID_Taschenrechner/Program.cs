ecce9a07fd57af315c0e822486873f344aaeba0f
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-22 11:23:32 +0200

    Mithilfe von Reflection die FreeFeatures und PremiumFeatures zur Laufzeit aus \bin\PlugIns laden und anwenden

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -1,8 +1,10 @@
 ﻿using CalculatorLogic;
-using FreeFeatures;
-using PremiumFeatures;
+using Models.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -14,8 +16,30 @@ namespace SOLID_Taschenrechner
         // Main(): Bootstrapping
         static void Main(string[] args)
         {
+            // Alle Assemblies aus dem PlugIn-Ordner laden
+            if (!Directory.Exists(@".\PlugIns"))
+                Directory.CreateDirectory(@".\PlugIns");
+
+            foreach (string file in Directory.GetFiles(@".\PlugIns"))
+            {
+                if (Path.GetExtension(file) == ".dll" || Path.GetExtension(file) == ".exe")
+                    Assembly.LoadFrom(file); // jede Assembly laden
+            }
+
+            // firmenname.projektname.Schichtname....
+            // ppedv.SolidTaschenrechner.Models
+            // ppedv.SolidTaschenrechner.Logic
+            var allAssemblies = AppDomain.CurrentDomain.GetAssemblies()
+                                                       .Where(x => x.FullName.StartsWith("Logic."))
+                                                       .SelectMany(x => x.GetTypes())
+                                                       .Where(x => typeof(ICalculationMethod).IsAssignableFrom(x) &&
+                                                                   x.IsInterface == false &&
+                                                                   x.IsAbstract == false)
+                                                       .Select(x => (ICalculationMethod)Activator.CreateInstance(x))
+                                                       .ToArray();
+
             var parser = new RegexParser();
-            var calculator = new ModularCalculator(new Addition(), new Subtraction(), new Multiplication(), new Division());
+            var calculator = new ModularCalculator(allAssemblies);
             new ConsoleUI(parser, calculator).Start();
 
             // Übung: DLL "PaidFeatures"
