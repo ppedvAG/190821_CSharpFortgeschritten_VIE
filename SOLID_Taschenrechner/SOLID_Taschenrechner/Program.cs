5294c19ea0fb65d475cbb1cec43ee2b6dd37b9ac
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-22 09:18:17 +0200

    Variante 1 ohne SOLID - Prinzipien

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -10,6 +10,26 @@ namespace SOLID_Taschenrechner
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
+            string input = Console.ReadLine();
+
+            string[] formula = input.Split();
+            int v1 = Convert.ToInt32(formula[0]);
+            int v2 = Convert.ToInt32(formula[2]);
+            string op = formula[1];
+
+            int result = 0;
+            if (op == "+")
+                result = v1 + v2;
+            else if (op == "-")
+                result = v1 - v2;
+            // ....
+
+            Console.WriteLine($"Das Ergebnis ist {result}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
