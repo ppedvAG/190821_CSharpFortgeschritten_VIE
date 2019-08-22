7d94669d969357bbf726355bc50987268c2f4a25
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-22 14:28:23 +0200

    Semaphore

---
+++ Semaphore_Demo/Semaphore_Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Semaphore_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SemaphoreZähler s_zähler = new SemaphoreZähler();
+
+            for (int i = 0; i < 500; i++)
+            {
+                new Thread(s_zähler.ZähleWas).Start();
+            }
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Semaphore_Demo/Semaphore_Demo/SemaphoreZähler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Semaphore_Demo
+{
+    class SemaphoreZähler
+    {
+        private static Semaphore semaphore = new Semaphore(10,10);
+        private static int zähler = 0;
+
+        public void ZähleWas()
+        {
+            semaphore.WaitOne();
+            zähler++;
+            Console.WriteLine(zähler);
+            zähler--;
+            semaphore.Release();
+        }
+
+    }
+}
