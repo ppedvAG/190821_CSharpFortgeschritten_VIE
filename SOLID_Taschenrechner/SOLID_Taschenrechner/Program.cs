054b234bc35b93fb050d68c9cae4da17f9bd5302
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-22 09:31:19 +0200

    Aufgaben in einzelne Klassen zerlegt (Single Responsibility Prinzip)

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -8,28 +8,72 @@ namespace SOLID_Taschenrechner
 {
     class Program
     {
+        // Main(): Bootstrapping
         static void Main(string[] args)
         {
+            new ConsoleUI().Start();
+        }
+    }
 
+    public struct Formula
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public string Operator { get; set; }
+    }
+
+    public class StringSplitParser
+    {
+        public Formula Parse(string input)
+        {
+            string[] formula = input.Split();
+
+            Formula output = new Formula();
+            output.Value1 = Convert.ToInt32(formula[0]);
+            output.Value2 = Convert.ToInt32(formula[2]);
+            output.Operator = formula[1];
+
+            return output;
+        }
+    }
+
+    public class SimpleCalculator
+    {
+        public int Calculate(Formula formula)
+        {
+            if (formula.Operator == "+")
+                return formula.Value1 + formula.Value2;
+            else if (formula.Operator == "-")
+                return formula.Value1 - formula.Value2;
+            // ....
+            else
+                throw new InvalidOperationException($"Der Operator {formula.Operator} wird nicht unterstützt");
+        }
+    }
+
+    public class ConsoleUI
+    {
+        // Definiert den Programm-Workflow
+        public void Start()
+        {
+            // UI
             Console.WriteLine("Bitte geben Sie die Formel ein:"); // "2 + 2"
             string input = Console.ReadLine();
 
-            string[] formula = input.Split();
-            int v1 = Convert.ToInt32(formula[0]);
-            int v2 = Convert.ToInt32(formula[2]);
-            string op = formula[1];
+            // Parsen
+            StringSplitParser parser = new StringSplitParser();
+            Formula formula = parser.Parse(input);
 
-            int result = 0;
-            if (op == "+")
-                result = v1 + v2;
-            else if (op == "-")
-                result = v1 - v2;
-            // ....
+            // Berechnen
+            SimpleCalculator calculator = new SimpleCalculator();
+            int result = calculator.Calculate(formula);
 
+            // UI
             Console.WriteLine($"Das Ergebnis ist {result}");
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
     }
+
 }
