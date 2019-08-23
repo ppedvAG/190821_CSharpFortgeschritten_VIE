9ff557c0f4f4344dec4dbe2670c5d49fb0428a17
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-23 09:59:43 +0200

    Init RESTService

---
+++ REST_Service_nutzen/REST_Service_nutzen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST_Service_nutzen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
