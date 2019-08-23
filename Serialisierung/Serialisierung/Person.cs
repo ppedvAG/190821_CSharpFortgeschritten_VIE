0b93c053d62be783b72c1ac88e7d27c3b4e62bb1
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-23 09:57:31 +0200

    Serialisierung

---
+++ Serialisierung/Serialisierung/Person.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialisierung
+{
+    //[Serializable]
+    //[XmlRoot("DasPersonenObjekt")]
+    public class Person
+    {
+        //[XmlAttribute("DerVorname")]
+        //[JsonProperty("vorname")]
+        public string Vorname { get; set; }
+        //[XmlAttribute("DerNachname")]
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        // [field: NonSerialized]
+        decimal Kontostand { get; set; }
+    }
+}
--- Serialisierung/Serialisierung/Program.cs
+++ Serialisierung/Serialisierung/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serialisierung
 {
@@ -10,8 +14,42 @@ namespace Serialisierung
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, }; // Kontostand = 100 };
+            Person p2 = new Person { Vorname = "Tom2", Nachname = "Ate", Alter = 10, }; // Kontostand = 100 };
+            Person p3 = new Person { Vorname = "Tom3", Nachname = "Ate", Alter = 10, }; //Kontostand = 100 };
 
+            Person[] personen = { p1, p2, p3 };
 
+            // Binär
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //FileStream stream = new FileStream("person.bin", FileMode.Create);
+
+            //formatter.Serialize(stream, personen);
+
+            //stream.Close();
+
+            //stream = new FileStream("person.bin", FileMode.Open);
+            //object p4 = formatter.Deserialize(stream);
+
+            // Console.WriteLine(p2.GetType());
+
+            // XML
+            // Limitierung: Klasse muss public sein
+
+            //XmlSerializer formatter = new XmlSerializer(typeof(Person));
+            //FileStream stream = new FileStream("person.xml", FileMode.Create);
+            //formatter.Serialize(stream, p1);
+            //stream.Close();
+
+            //stream = new FileStream("person.xml", FileMode.Open);
+            //object personXML = formatter.Deserialize(stream);
+            //stream.Close();
+
+            // JSON
+
+            File.WriteAllText("person.json",JsonConvert.SerializeObject(personen));
+
+            List<Person> personJson = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText("person.json"));
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
