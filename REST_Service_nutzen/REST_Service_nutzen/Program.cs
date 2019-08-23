82ea9f43bfffaf09b8c4de60574e81f9ee2d98ce
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-23 10:08:12 +0200

    Kleines REST-Beispiel

--- REST_Service_nutzen/REST_Service_nutzen/Program.cs
+++ REST_Service_nutzen/REST_Service_nutzen/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,24 @@ namespace REST_Service_nutzen
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args) // seit C# 7.3
         {
+            // 1) JSON-String herunterladen
+
+            string json;
+            using (HttpClient client = new HttpClient())
+            {
+                json = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+            }
+
+            Console.WriteLine(json);
+
+            User[] users = JsonConvert.DeserializeObject<User[]>(json);
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Id}: {user.Title}");
+            }
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
